c81c74d8acacca51f9c91ffb720ec24efaf0e94a
Mariana Assis <mariana@bisect.pt>
2026-06-05 21:50:06 +0100

    Add USB senders and receivers

--- Development/cmake/NmosCppLibraries.cmake
+++ Development/cmake/NmosCppLibraries.cmake
@@ -1264,6 +1264,7 @@ set(NMOS_CPP_NMOS_HEADERS
     nmos/transfer_characteristic.h
     nmos/transport.h
     nmos/type.h
+    nmos/usb.h
     nmos/version.h
     nmos/video_jxsv.h
     nmos/vpid_code.h
